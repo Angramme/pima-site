9ea2098ec7bb12467bd2127034694a000b4d0b13
Angramme <kacpero376@gmail.com>
2023-07-25 17:45:30 +0200

    menu bestest

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,13 +1,14 @@
 
 <div id="cont">
     <div id="nav-cont">
-        <div id="gradient"></div>
         <nav>
-            Navigation <br/>
-            <a href="/"> acceuil </a> |
-            <a href="/restricted/anciens"> anciens </a> |
-            <a href="/restricted/faq"> FAQ </a>
+            &nbsp;&nbsp;
+            <a>PIMA</a> &nbsp; &nbsp;
+            <a href="/"> acceuil </a> &nbsp;
+            <a href="/restricted/anciens"> anciens </a> &nbsp;
+            <a href="/restricted/faq"> FAQ </a> &nbsp;
         </nav>
+        <!-- <div id="gradient"></div> -->
     </div>
     <slot/>
     <div id="spacer"></div>
@@ -27,26 +28,31 @@
         margin-top: 70px;
         padding: 20px;
     }
-    #gradient{
+    /* #gradient{
         width: 100%;
         height: 100px;
-        background: linear-gradient(rgba(245, 245, 220, 0), beige);
-    }
+        background: linear-gradient(beige, rgba(245, 245, 220, 0));
+    } */
     #nav-cont{
         width: 75vh;
         max-width: 100vw;
         position: fixed;
-        bottom: 0;
+        top: 0;
     }
     nav{
-        border-top: solid 1px black;
-        padding-top: 10px;
-        padding-bottom: 20px;
+        border-bottom: solid 1px black;
+        padding-top: 15px;
+        padding-bottom: 15px;
         background: beige;
+        background-image: radial-gradient(gray 1px, transparent 0);
+        background-size: 5px 5px;
+        background-position: -19px -19px;
     }
     nav *{
         text-decoration: none;
         font-size: larger;
+        background: beige;
+        padding: 2px;
     }
     #spacer{
         margin-top: 200px;
