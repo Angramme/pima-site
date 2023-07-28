56cba717f8c5d4cb3dc5354513a1dfec2e55ffa4
Angramme <kacpero376@gmail.com>
2023-07-28 17:04:03 +0200

    bruh

--- src/routes/restricted/user/+page.server.js
+++ src/routes/restricted/user/+page.server.js
@@ -1,5 +1,5 @@
 import prisma from '$lib/prisma';
-import { check_password, end_session } from '$lib/sessions';
+import { check_password, end_session, hash_password } from '$lib/sessions';
 import { redirect } from '@sveltejs/kit';
 
 /** @type {import('./$types').PageServerLoad} */
@@ -16,12 +16,12 @@ export async function load({ locals }) {
             email: true,
             contact: true,
             
-            description: true,
-            nationalite: true,
             nom: true,
             prenom: true,
-            login: true,
+            nationalite: true,
+            description: true,
             
+            login: true,
             admin: true,
             createdAt: true,
             updatedAt: true,
@@ -37,22 +37,100 @@ export async function load({ locals }) {
 export const actions = {
     disconnect: async ({ cookies }) => {
         end_session(cookies);
-        throw redirect(303, "/");
+        return { disconnected: true };
     },
-    reset_pwd: async ({ request, cookies, locals }) => {
+    pwd_chg: async ({ request, locals }) => {
         const data = await request.formData();
         const old_pwd = data.get("old_pwd");
         const new_pwd = data.get("new_pwd");
-        if(!locals.user || !old_pwd) return { reset_pwd: true, missing: true };
+        if(!locals.user || !old_pwd || !new_pwd) return { pwd_rst_missing: true };
         const user_id = await check_password(locals.user.login, old_pwd.toString());
-        if(!user_id) return { reset_pwd: true, incorrect: true };
+        if(!user_id) return { pwd_rst_incorrect: true };
         await prisma.user.update({
             where: { id: user_id },
-            data: { password: new_pwd?.toString() },
+            data: { password: hash_password(locals.user.login, new_pwd.toString()) },
         });
-        return { reset_pwd: true, success: true };
+        return { pwd_rst_success: true };
     },
-    update_data: async ({}) => {
+    update_data: async ({ request, locals }) => {
+        const data = await request.formData();
 
+        const keys_to_update = [
+            "admis",
+            "choisi",
+            "grad_year",
+            "email",
+            "contact",
+            "nom",
+            "prenom",
+            "nationalite",
+            "description",
+        ];
+        const list_entries = [
+            "nationalite",
+            "contact",
+            "admis",
+        ];
+        const number_entries = [ "grad_year" ];
+
+        if(!locals.user) return { update_failure: "user logged out!" };
+        if(keys_to_update.some(k=>!data.has(k))) return { update_failure: "Invalid form data submitted!" };
+
+        let prisma_data = Object();
+        for(let k of keys_to_update) prisma_data[k] = data.get(k)?.toString().trim();
+        /// @ts-ignore
+        for(let k of list_entries) prisma_data[k] = prisma_data[k].split(",").map(x=>x.trim());
+        for(let k of number_entries) prisma_data[k] = Number(prisma_data[k]);
+
+        console.log(prisma_data);
+
+        await prisma.user.update({
+            where: { id: locals.user.id },
+            data: prisma_data,
+        })
+        
+        return { update_success: true };
+    },
+    delete_account: async ({ locals })=>{
+        if(!locals.user) return { delete_failure: "Vous n'etez pas connectés" };
+        await prisma.session.deleteMany({
+            where: { user_id: locals.user.id }
+        });
+        await prisma.user.delete({
+            where: { id: locals.user.id }
+        });
+        return { delete_success: true };
+    },
+    create_account: async ({ locals, request }) => {
+        if(!locals.user) return { creation_failure: "user logged out" };
+        if(!locals.user.admin) return { creation_failure: "user does not have permissions" };
+
+        const data = await request.formData();
+        if(!data.has("prenom") || !data.has("nom") || !data.has("grad_year") || !data.has("password"))
+            return { creation_failure: "form data incomplete" };
+
+        const prenom = data.get("prenom")?.toString() || ""; 
+        const nom = data.get("nom")?.toString() || ""; 
+        let login = `${nom.toLowerCase()}-${prenom.toLowerCase()}`; 
+        const grad_year = Number(data.get("grad_year")?.toString()) || 0; 
+        const password = hash_password(login, data.get("password")?.toString() || "");
+
+        // make login unique
+        const logins = (await prisma.user.findMany({
+                where: { login: {
+                    contains: login
+                }},
+                select: { login: true }
+            }))
+            .map(({login})=>login);
+        let i = 0;
+        while(logins.includes(login))
+            login = `${nom.toLowerCase()}-${prenom.toLowerCase()}-${i++}`;
+
+        await prisma.user.create({
+            data: { prenom, nom, login, grad_year, choisi: "? 😴", password }
+        });
+
+        return { creation_success: true, new_account_login: login }; 
     },
 };
--- src/routes/restricted/user/+page.svelte
+++ src/routes/restricted/user/+page.svelte
@@ -1,65 +1,124 @@
 <script>
-  import { invalidate } from '$app/navigation';
+  import { browser } from '$app/environment';
+  import { goto, invalidate } from '$app/navigation';
 
     /** @type {HTMLInputElement}*/
     let delete_account_btn;
     /** @type {any}*/
     let user_agreed;
 
+    /** @type {import('./$types').PageData} */
     export let data;
 
     /** @type {import('./$types').ActionData} */
     export let form;
 
 
-    const big_box = ["admis", "contact", "description"];
+    $: user = data.user;
+    $: sections = !user ? null : [
+        {n:"Ecoles", l:[
+            {k:"admis", l:"admis dans", v:user.admis, t:"textlist", ro:false},
+            {k:"choisi", l:"choisi", v:user.choisi, t:"text", ro:false},
+            {k:"grad_year", l:"année fin L3", v:user.grad_year, t:"number", ro:false},
+        ]},
+        {n:"Contact", l:[
+            {k:"email", l:"email", v:user.email, t:"email", ro:false},
+            {k:"contact", l:"autres moyens de contacter", v:user.contact, t:"textlist", ro:false},
+        ]},
+        {n:"Infos personelles", l:[
+            {k:"nom", l:"nom de famille (facultatif)", v:user.nom, t:"text", ro:false},
+            {k:"prenom", l:"prénom", v:user.prenom, t:"text", ro:false},
+            {k:"nationalite", l:"nationalité/s", v:user.nationalite, t:"textlist", ro:false},
+            {k:"description", l:"description et conseils", v:user.description, t:"longtext", ro:false},
+        ]},
+        {n:"Autres (Lecture seule)", l:[
+            {k:"login", l:"login", v:user.login, t:"text", ro:true},
+            {k:"admin", l:"admin privileges", v:user.admin, t:"text", ro:true},
+            {k:"createdAt", l:"date création", v:user.createdAt, t:"date", ro:true},
+            {k:"updatedAt", l:"derniere mise a jour", v:user.updatedAt, t:"date", ro:true},
+        ]},
+    ];
 
-    /** @param {string} type */
-    const type_to_input = (type)=>{
-        switch(type){
-            case "number": return "number"
-            case "string": return "text"
-        }
-    }
-
-    $: data_entries = Object.entries(data.user || {})
-        .filter(([k, _])=>!["admin", "createdAt", "updatedAt"].includes(k));
+    // invalidate load dependent on locals.user when disconnected
+    $: form?.disconnected, browser && invalidate("user:update")
 
     /** @type {boolean}*/
     let new_pwd_val;
     /** @type {boolean}*/
     let con_pwd_val;
+
+    /** @type {HTMLFormElement}*/
+    let delete_account_form;
+    const delete_account_now = async ()=>{
+        const yes = confirm("Veuillez vous vraiment supprimer votre compte? Attention cette opération n'est pas réversible.");
+        if(!yes) return;
+        delete_account_form.submit();
+    }
+    $: form?.delete_failure, browser && form?.delete_failure && alert("Erreur serveur: " + form?.delete_failure);
+    $: form?.delete_success, browser && invalidate("user:update");
+
+    /** @type {HTMLInputElement}*/
+    let create_pwd;
+
+    const generate_password = (
+        length = 30,
+        wishlist = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz~!@-#$'
+    ) =>
+        Array.from(crypto.getRandomValues(new Uint32Array(length)))
+            .map((x) => wishlist[x % wishlist.length])
+            .join('');
 </script>
 
 <h1>Donnes et parametres utilisateur</h1>
 
+<!-- Server message sesction -->
+{#if form?.pwd_rst_missing}<p class="error">Manque de mot de passe!</p>{/if}
+{#if form?.pwd_rst_incorrect}<p class="error">Mot de passe incorrect!</p>{/if}
+{#if form?.pwd_rst_success}<p class="success">Changement réussi!</p>{/if}
+{#if form?.disconnected} <p class="success">Déconnexion réussie!</p> {/if}
+{#if form?.update_success}<p class="success">Mise a jour réussie!</p>{/if}
+{#if form?.update_failure}<p class="error">Erreur de mise à jour: "{form.update_failure}""</p>{/if}
+
+
 <form method="POST" action="?/disconnect">
     <fieldset>
+        {#if form?.disconnected} <p class="success">Déconnexion réussie!</p> {/if}
         <legend>Deconnexion</legend>
-        <input type="submit" value="Deconnexion" on:click={()=>invalidate("user:update")}/>
+        <input type="submit" value="Deconnexion"/>
     </fieldset>
 </form>
 
 <form method="POST" action="?/update_data">
     <fieldset>
         <legend>Mes donnes</legend>
-        {#if !data.user}
+        {#if !sections}
             <p class="error">Erreur interne! Donnes indisponibles!</p>
-        {/if}
-        <table>
-            {#each data_entries as [k, v]}
-                <tr>
-                    <td><label for={`input_${k}`}>{k}</label></td>
-                    {#if big_box.includes(k)}
-                    <td><textarea name={k} id={`input_${k}`} rows="3" cols="40">{v}</textarea></td>
-                    {:else}
-                    <td><input name={k} id={`input_${k}`} type={type_to_input(typeof v)} value={v}/></td>
-                    {/if}
-                </tr>
+        {:else}
+            {#each sections as {n, l: list}}
+                <fieldset>
+                    <legend>{n}</legend>
+                    <table>
+                        {#each list as {k: key, l: label, v: value, t: type, ro: read_only}}
+                        <tr>
+                            <td><label for={`input_${key}`}>{label}</label></td>
+                            {#if type=="textlist" || type=="longtext"}
+                                <td>
+                                    {#if type=="textlist"} <span class="info">lister en séparant par des ","</span><br/> {/if}
+                                    <textarea name={key} id={`input_${key}`} rows={type=="textlist" ? 3 : 5} cols="40">{value}</textarea>
+                                </td>
+                            {:else}
+                                <td><input name={key} id={`input_${key}`} type={type} value={value} readonly={read_only}/></td>
+                            {/if}
+                        </tr>
+                        {/each}
+                    </table>
+                </fieldset>
             {/each}
-        </table>
+        {/if}
         <fieldset>
             <legend>Soumettre</legend>
+            {#if form?.update_success}<p class="success">Mise a jour réussie!</p>{/if}
+            {#if form?.update_failure}<p class="error">Erreur de mise à jour: "{form.update_failure}""</p>{/if}
             <input bind:checked={user_agreed} id="agreement" type="checkbox"/>
             <label for="agreement">J'ai pris conaissance de la reglementation <a href="/reglementation" target="_blank">ici</a> et je donne mon accord pour utilisation de mes donnees plus haut.</label><br/>
             <input type="submit" disabled={!user_agreed}/>
@@ -67,14 +126,12 @@
     </fieldset>
 </form>
 
-<form>
+<form method="POST" action="?/pwd_chg">
     <fieldset id="pwd_chg">
         <legend>Reinitialisation mot de passe</legend>
-        {#if form?.reset_pwd}
-            {#if form?.missing}<p class="error">Manque de mot de passe!</p>{/if}
-            {#if form?.incorrect}<p class="error">Mot de passe incorrect!</p>{/if}
-            {#if form?.success}<p class="success">Changement réussi!</p>{/if}
-        {/if}
+        {#if form?.pwd_rst_missing}<p class="error">Manque de mot de passe!</p>{/if}
+        {#if form?.pwd_rst_incorrect}<p class="error">Mot de passe incorrect!</p>{/if}
+        {#if form?.pwd_rst_success}<p class="success">Changement réussi!</p>{/if}
         <table>
             <tr>
                 <td><label for="old_pwd">Ancien mot de passe</label></td>
@@ -96,27 +153,86 @@
     </fieldset>
 </form>
 
-<form>
-    <fieldset id="danger">
+<form bind:this={delete_account_form} method="POST" action="?/delete_account">
+    <fieldset class="danger">
         <legend>Danger Zone</legend>
+        {#if form?.delete_failure}<p class="error">Erreur serveur: "{form.delete_failure}"</p>{/if}
+        {#if form?.delete_success}<p class="success">Compte supprimé!</p>{/if}
         <input type="button" value="Debloquer" on:click={()=>{delete_account_btn.disabled = false;}}/>
-        <input bind:this={delete_account_btn} type="button" value="Supprimer ce compte" disabled/>
+        <input bind:this={delete_account_btn} on:click={delete_account_now} type="button" value="Supprimer ce compte" disabled/>
     </fieldset>
 </form>
+
+{#if user?.admin}
+    <form method="POST" action="?/create_account">
+        <fieldset class="admin">
+            <legend>Admin</legend>
+            <fieldset>
+                <legend>Créer un compte</legend>
+                {#if form?.creation_failure}<p class="error">Erreur serveur: "{form.creation_failure}"</p>{/if}
+                {#if form?.creation_success}<p class="success">Creation réussie! login: {form.new_account_login} </p>{/if}        
+                <table>
+                    <tr>
+                        <td><label for="create_prenom">prenom</label></td>
+                        <td><input name="prenom" id="create_prenom" type="text"></td>
+                    </tr>
+                    <tr>
+                        <td><label for="create_nom">nom</label></td>
+                        <td><input name="nom" id="create_nom" type="text"></td>
+                    </tr>
+                    <tr>
+                        <td><label for="create_grad_year">année fin L3</label></td>
+                        <td><input name="grad_year" id="create_grad_year" type="number"></td>
+                    </tr>
+                    <tr>
+                        <td><label for="create_pwd">password</label></td>
+                        <td><input bind:this={create_pwd} name="password" id="create_pwd" type="string" value={generate_password()}></td>
+                        <td><input type="button" on:click={()=>navigator.clipboard.writeText(create_pwd.value)} value="copy"/></td>
+                        <td><input type="button" on:click={()=>create_pwd.value=generate_password()} value="regenerate"/></td>
+                    </tr>
+                    <tr>
+                        <td></td>
+                        <td><input type="submit" value="Créer"></td>
+                    </tr>
+                </table>
+                <p class="info">Copiez le mot de passe, cliquez sur creer puis copiez le login.</p>
+            </fieldset>
+        </fieldset>
+    </form>
+{/if}
 
 <style>
     fieldset{
         margin-top: 20px;
     }
-    #danger{
+    input:read-only[type~="text"], input:read-only[type~="date"], textarea:read-only{
+        background: rgb(196, 196, 196);
+    }
+    .admin{
+        background-color: aquamarine;
+    }
+    .danger{
         background-color: rgb(255, 95, 95);
     }
-    #danger legend{
+    .danger legend{
         color: red;
         font-weight: bold;
         background: beige;
     }
+    .error{
+        color: red;
+    }
+    .success{
+        color: green;
+    }
+    .info{
+        font-style: italic;
+        color: blue;
+    }
     table{
         border-spacing: 7px;
+    }
+    table tr td:first-child {
+        width: 100px;
     }
 </style>
