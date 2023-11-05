48e79b37f66bccab0fbe2d28398fd483c1873ecf
Angramme <34948473+Angramme@users.noreply.github.com>
2023-11-05 16:56:55 +0100

    design improvements

--- src/lib/components/Rolldown.svelte
+++ src/lib/components/Rolldown.svelte
@@ -1,5 +1,5 @@
 <script>
-    import { afterUpdate } from "svelte";
+    import { afterUpdate, onMount } from "svelte";
 
     /** @type {boolean} */
     export let default_open = false;
@@ -7,34 +7,46 @@
     export let closed_height = "15vh";
 
     let open = default_open;
-    let plus_needed = true;
+    let plus_needed = false;
 
     /** @type {HTMLElement}*/
     let main;
 
     afterUpdate(()=>{
-        plus_needed = open || main.scrollHeight != main.clientHeight;
+        plus_needed = plus_needed || open || main.scrollHeight != main.clientHeight;
     });
+
+    const roll = ()=>{
+        open=!open;
+        setTimeout(()=>{
+            main.scrollIntoView({
+                behavior: 'smooth',
+                block: 'center',
+                inline: 'start',
+            });
+        }, 200);
+    };
 </script>
 
-<div bind:this={main} class="main" style={open ? "" : `max-height: ${closed_height};`}>
+<div bind:this={main} class="main" style={open ? "" : `max-height: ${closed_height};overflow: hidden !important;`}>
     <slot>
         No content provided...
     </slot>
 </div>
 {#if plus_needed}
     <div class="plus">
-        <button on:click={()=>open=!open}>{open ? "- cacher -" : "+ afficher plus +"}</button>
+        <button on:click={roll}>{open ? "- cacher -" : "+ afficher plus +"}</button>
     </div>
 {/if}
 
 <style>
     div.main{
-        overflow-y: scroll;
-        overflow-x: scroll;
+        overflow: scroll;
+        max-height: 90vh;
+        transition: max-height 0.2s ease-in-out;
     }
     .plus{
-        margin-top: 0.5rem;
+        margin-top: 0.2rem;
         text-align: center;
     }
 </style>
